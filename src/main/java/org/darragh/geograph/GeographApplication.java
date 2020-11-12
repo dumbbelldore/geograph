94b2ef58c9a5e8ec451df52ae6c805eb57f7ead7
Jordan Penn-Geddes <jgeddes101@gmail.com>
2020-11-12 11:44:20 -0500

    Setup initial Spring Boot framework.

---
+++ src/main/java/org/darragh/geograph/GeographApplication.java
@@ -0,0 +1,13 @@
+package org.darragh.geograph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeographApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeographApplication.class, args);
+	}
+
+}
